10571328334493442af6899c071c1024fb3574b9
Nathan Anderson <SniperSmiley@gmail.com>
2021-08-24 16:51:25 -0700

    Not making much progress today

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,23 +5,36 @@ using UnityEngine.InputSystem;
 public class CameraController : MonoBehaviour
 {
     public float panSpeed = 20f;
-    public GameObject camera;
+    public float rotateSpeed = 20f;
+    public GameObject cameraUsed;
     private Vector2 direction;
+    private Vector2 rotation;
     void Awake()
     {
     }
-    // Update is called once per frame
     public void CameraMove(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
-        Debug.Log(direction);
+        //Debug.Log(direction);
 
     }
+    public void CameraRotate(InputAction.CallbackContext context)
+    {
+        rotation = context.ReadValue<Vector2>();
+        //Debug.Log(rotation);
+    }
+
     private void Update()
     {
-        Vector3 pos = camera.transform.position;
+        Vector3 pos = cameraUsed.transform.position;
         pos.x += direction.x * panSpeed * Time.deltaTime;
         pos.z += direction.y * panSpeed * Time.deltaTime;
-        camera.transform.position = pos;
+        cameraUsed.transform.position = pos;
+
+        var rotate = transform.rotation;
+        rotate.z += rotation.x * rotateSpeed * Time.deltaTime;
+        //rotate.z += rotation.y * rotateSpeed * Time.deltaTime;
+        //transform.rotation = rotate;
+
     }
 }
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    public void Pickup(InputAction.CallbackContext context)
+    {
+        //rotation = context.ReadValue<Vector2>();
+        //Debug.Log(rotation);
+    }
+    public void Position(InputAction.CallbackContext context)
+    {
+        //rotation = context.ReadValue<Vector2>();
+        //Debug.Log(rotation);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
